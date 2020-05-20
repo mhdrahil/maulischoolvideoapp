e49f248a7944f45b992a1a93a13f05e86876d6ec
mhdrahil <mhd.rahil@gmail.com>
2020-05-21 02:50:50 +0530

    initial commit

--- server.js
+++ server.js
@@ -1,27 +1,52 @@
-// Node Get ICE STUN and TURN list
-let o = {
-    format: "urls"
-};
+var app = require("express")();
+var http = require("http").Server(app);
+var fs = require("fs");
+var io = require("socket.io")(http);
 
-let bodyString = JSON.stringify(o);
-let https = require("https");
-let options = {
-    host: "global.xirsys.net",
-    path: "/_turn/videochatapp77",
-    method: "PUT",
-    headers: {
-        "Authorization": "Basic " + Buffer.from("mhdrahil:81b2acd8-9a74-11ea-86d8-0242ac150002").toString("base64"),
-        "Content-Type": "application/json",
-        "Content-Length": bodyString.length
-    }
-};
-let httpreq = https.request(options, function(httpres) {
-    let str = "";
-    httpres.on("data", function(data){ str += data; });
-    httpres.on("error", function(e){ console.log("error: ",e); });
-    httpres.on("end", function(){ 
-        console.log("ICE List: ", str);
-    });
+// handling files
+app.get('/', onRequest);
+app.get('/js/client.js', function(req, res) {
+  res.sendFile(__dirname + "/js/client.js");
 });
-httpreq.on("error", function(e){ console.log("request error: ",e); });
-httpreq.end();
+
+app.get('/js/room.js', function(req, res) {
+  res.sendFile(__dirname + "/js/room.js");
+});
+
+http.listen(process.env.PORT || 3000, function() {
+  console.log('server started');
+})
+
+//404 response
+function send404(response) {
+  response.writeHead(404, {"Content-Type" : "text/plain"});
+  response.write("Error 404: Page not found");
+  response.end();
+}
+function appGet(urlPath, fileExtension){
+    app.get(urlPath, function(req, res){
+      var path = __dirname + fileExtension;
+      console.log("loading " + path);
+       res.sendFile(__dirname + fileExtension);
+    });
+}
+function onRequest(request, response) {
+  var room = request.query.room;
+  console.log("room id: " + room);
+  fs.createReadStream("./index.html").pipe(response);
+}
+
+
+// someone connects to a room
+io.on('connection', function(socket) {
+  console.log("a user connected");
+  
+  //relay the stream
+  socket.on("message", function(message) {
+    socket.broadcast.emit("message", message);
+  });
+
+  socket.on('disconnect', function () {
+    console.log("user disconnected");
+  });
+});
