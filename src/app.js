2ee7a2c3b0108f23ab7c2708d355a00bb5dbf765
mhdrahil <mhd.rahil@gmail.com>
2020-05-21 02:09:48 +0530

    initial commit

--- src/app.js
+++ src/app.js
@@ -7,8 +7,9 @@ let stream = require('./ws/stream');
 let path = require('path');
 let favicon = require('serve-favicon')
 
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
 
 app.get('/', (req, res)=>{
     res.sendFile(__dirname+'/index.html');
