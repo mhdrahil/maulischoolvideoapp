d100cc1e11cdc8789525524e97121a707c4c7b4f
mhdrahil <mhd.rahil@gmail.com>
2020-05-21 01:14:21 +0530

    another commit

--- src/app.js
+++ src/app.js
@@ -1,16 +1,19 @@
+let express = require('express');
+let app = express();
+let server = require('http').Server(app);
+let io = require('socket.io')(server);
+let stream = require('./ws/stream');
+let path = require('path');
+let favicon = require('serve-favicon')
+
+app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+
+app.get('/', (req, res)=>{
+    res.sendFile(__dirname+'/index.html');
+});
 
 
-// Write Javascript code here 
+io.of('/stream').on('connection', stream);
 
-var http = require('http'); 
-var fs = require('fs'); // to get data from html file 
-
-
-http.createServer(function (req, res) { 
-	res.writeHead(200, { 'Content-Type': 'index/html' }); 
-
-	
-
-}).listen(process.env.PORT || 3000, function () { 
-	console.log("SERVER STARTED PORT: 3000"); 
-}); 
+server.listen(3000);
