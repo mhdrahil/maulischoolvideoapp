a902d034e49616a7a764d4da1333eba4b39c934d
mhdrahil <mhd.rahil@gmail.com>
2020-05-21 14:12:03 +0530

    another commit

--- src/app.js
+++ src/app.js
@@ -18,6 +18,4 @@ app.get('/', (req, res)=>{
 
 io.of('/stream').on('connection', stream);
 
-app.listen(PORT, () => {
-    console.log(`Our app is running on port ${ PORT }`);
-});
+server.listen(3000);
